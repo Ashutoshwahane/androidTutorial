d65f2be800fee663ebdc1fcd65d00d0f89ad387f
willskhalifa <ashutosh.wahane10@gmail.com>
2019-12-28 10:49:26 +0530

    customize to TMV app, icons and logo

---
+++ NavDrawer/app/src/main/java/com/example/navdrawer/HelpViewModel.java
@@ -0,0 +1,7 @@
+package com.example.navdrawer;
+
+import androidx.lifecycle.ViewModel;
+
+public class HelpViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ NavDrawer/app/src/main/java/com/example/navdrawer/help.java
@@ -0,0 +1,36 @@
+package com.example.navdrawer;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class help extends Fragment {
+
+    private HelpViewModel mViewModel;
+
+    public static help newInstance() {
+        return new help();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.help_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(HelpViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
