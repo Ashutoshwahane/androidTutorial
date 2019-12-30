b03d636510a6b8a93d9144c9b3c06f347ddec587
willskhalifa <ashutosh.wahane10@gmail.com>
2019-12-30 14:47:07 +0530

    implement API Interface

---
+++ JSONParse/app/src/main/java/APIInterface.java
@@ -0,0 +1,24 @@
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+interface  APIInterface {
+
+    @GET("/api/unknown")
+    Call<JSONObject> doGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+//
+//    @GET("/api/users?")
+//    Call<UserList> doGetUserList(@Query("page") String page);
+//
+//    @FormUrlEncoded
+//    @POST("/api/users?")
+//    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
